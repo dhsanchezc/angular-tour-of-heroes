8ec4bf7b5d87036cc619da383da1ff9f4b18ecb3
Diego Sánchez <disan100@gmail.com>
2021-07-07 16:48:46 -0500

    d

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   heroes = HEROES;
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
